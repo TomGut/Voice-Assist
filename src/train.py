44642c9f32d1b0dee4c00ffe947253a2f9ec33af
tom <t-gutowski@o2.pl>
2022-02-09 19:09:43 +0100

    implemented DataSet and fixed numpy error with BitLoader

--- src/train.py
+++ src/train.py
@@ -2,6 +2,9 @@
 import json
 
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
 
 from nltk_tools import Nltk_Tools
 
@@ -38,3 +41,21 @@ for (pattern_sentence, tag) in xy:
 
 x_train = np.array(x_train)
 y_train = np.array(y_train)
+
+
+class Chat_Dataset(Dataset):
+    def __init__(self) -> None:
+        self.n_samples = len(x_train)
+        self.x_data = x_train
+        self.y_data = y_train
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
+
+
+dataset = Chat_Dataset()
+train_loader = DataLoader(dataset=dataset, batch_size=8,
+                          shuffle=True, num_workers=2)
