d465125bf600ef7884b5cba75ec53d1b5d69e327
tom <t-gutowski@o2.pl>
2022-02-09 20:43:05 +0100

    implemented model save and added trained pth

--- src/train.py
+++ src/train.py
@@ -7,8 +7,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 
-from nltk_tools import Nltk_Tools
 from model import Neural_Model
+from nltk_tools import Nltk_Tools
 
 with open('intents/intents.json', 'r') as f:
     data = json.load(f)
@@ -90,7 +90,21 @@ for epoch in range(epoch_num):
         opt.zero_grad()
         loss.backward()
         opt.step()
-    
-    if (epoch +1) % 100 == 0:
+
+    if (epoch + 1) % 100 == 0:
         print(f'epoch {epoch+1}/{epoch_num}, loss={loss.item():.4f}')
-        print(f'final loss, loss={loss.item():.4f}')       
+
+print(f'final loss, loss={loss.item():.4f}')
+
+# Save trained model
+data = {
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "output_size": output_size,
+    "hidden_size": hidden_size,
+    "all_words": all_words,
+    "tags": tags
+}
+
+FILE = 'data.pth'
+torch.save(data, FILE)
