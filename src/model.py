3516292f0dbffe87a1d4c282abc936129910425b
tom <t-gutowski@o2.pl>
2022-02-09 20:26:37 +0100

    implemented training loop

--- src/model.py
+++ src/model.py
@@ -2,12 +2,13 @@ import torch
 import torch.nn as nn
 
 
-class Model(nn.Module):
+class Neural_Model(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
+        super(Neural_Model, self).__init__()
         self.l1 = nn.Linear(input_size, hidden_size)
         self.l2 = nn.Linear(hidden_size, hidden_size)
         self.l3 = nn.Linear(hidden_size, num_classes)
-        self.relu = nn.ReLu()
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         out = self.l1(x)
--- src/train.py
+++ src/train.py
@@ -1,10 +1,14 @@
 
 import json
+import random
 
 import numpy as np
+import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 
 from nltk_tools import Nltk_Tools
+from model import Neural_Model
 
 with open('intents/intents.json', 'r') as f:
     data = json.load(f)
@@ -57,3 +61,36 @@ class Chat_Dataset(Dataset):
 dataset = Chat_Dataset()
 train_loader = DataLoader(dataset=dataset, batch_size=8,
                           shuffle=True, num_workers=2)
+
+
+hidden_size = 8
+output_size = len(tags)
+input_size = len(x_train[0])
+learning_rate = 0.001
+epoch_num = 1000
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Neural_Model(input_size, hidden_size, output_size).to(device)
+
+# Loss and optimizer
+crit = nn.CrossEntropyLoss()
+opt = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Training loop
+for epoch in range(epoch_num):
+    for(words, labels) in train_loader:
+        words = words.to(device, dtype=torch.float)
+        labels = labels.to(device)
+
+        # Forward
+        outputs = model(words)
+        loss = crit(outputs, labels)
+
+        # Backoward and optimizer step
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+    
+    if (epoch +1) % 100 == 0:
+        print(f'epoch {epoch+1}/{epoch_num}, loss={loss.item():.4f}')
+        print(f'final loss, loss={loss.item():.4f}')       
