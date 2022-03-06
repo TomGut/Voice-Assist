5686760a039f025e7a3db6f05eb60a23533c1201
tom <t-gutowski@o2.pl>
2022-03-06 13:31:20 +0100

    docs(nltk_tools): added comments and docstrings

--- src/nltk_tools.py
+++ src/nltk_tools.py
@@ -2,6 +2,7 @@ from array import array
 import nltk
 import numpy as np
 
+# In case of missing Punkt Sentence Tokenizer uncomment line below.
 # nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 
@@ -9,16 +10,54 @@ _stemmer = PorterStemmer()
 
 
 class Nltk_Tools:
-    def tokenize(self, sentence):
+    def tokenize(self, sentence: str) -> str:
+        """Separation of sentence into smaller units (tokens) to be classifed
+        later on in bag of words.
+
+        Args:
+            sentence (str): text to be tokenized
+
+        Returns:
+            str: tokenized sentence
+        """
         return nltk.word_tokenize(sentence)
 
-    def stem(self, word):
+    def stem(self, word: str) -> str:
+        """Extraction of word base - removing affixes at the end of word.
+
+        Args:
+            word (str): word to be stemmed.
+
+        Returns:
+            str: stemmed word.
+        """
         return _stemmer.stem(word.lower())
 
-    def bag_of_words(self, tokenized_sentence, all_words) -> array:
+    def bag_of_words(
+        self, tokenized_sentence: str, all_words: array
+    ) -> array:
+        """Text Classification.
+        Take all the words in sentence, then count the number of occurrences
+        of each word.
+        After finding the number of occurrences of each word, we will choose
+        a certain number of words that appeared more often than other words.
+        These words are the features for our classification problem.
+        Then when it comes to classification, the classifier, will check what
+        are the words and which intent they belong to later on.
+
+        Args:
+            tokenized_sentence (str): tokenized words
+            all_words (array): all words to be put in the bag
+
+        Returns:
+            array: bag of words - numbers indicating if word exists in the
+            sentence - words classification (match to pattern in intents).
+        """
         tokenized_sentence = [self.stem(w) for w in tokenized_sentence]
         bag = np.zeros(len(all_words), dtype=np.float)
         for idx, w in enumerate(all_words):
             if w in tokenized_sentence:
+                # Word classification in array to check if word shows in sentence.
+                # 1 stands for - exist.
                 bag[idx] = 1.0
         return bag
