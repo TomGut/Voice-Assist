ead96fccc2de4eaa1f5be9dffc4a10088aa47440
tom <t-gutowski@o2.pl>
2022-03-06 21:21:58 +0100

    docs, style(time_skill): added doc strings and fixed return types

--- src/time_skill.py
+++ src/time_skill.py
@@ -1,5 +1,10 @@
-from datetime import datetime
+""" 
+Functionality to provide date, time and week no.
+"""
+
 import locale
+from datetime import datetime
+from typing import Optional
 
 
 class Time_Skill:
@@ -7,14 +12,14 @@ class Time_Skill:
         locale.setlocale(locale.LC_ALL, "")
         self._today = datetime.today()
 
-    def get_date(self) -> str:
+    def get_date(self) -> Optional[datetime]:
         date = self._today.strftime("%A, %-d %B")
         return date
 
-    def get_week_number(self) -> str:
+    def get_week_number(self) -> Optional[datetime]:
         week = self._today.strftime("%-W")
         return week
 
-    def get_time(self) -> str:
+    def get_time(self) -> Optional[datetime]:
         hour = self._today.strftime("%X")
         return hour
