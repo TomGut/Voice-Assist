930b24edd141e2e21bc25a6c7be34fd115828b3e
tom <t-gutowski@o2.pl>
2022-03-12 18:44:03 +0100

    test(time_skill): added locale and datetime call test

--- tests/time_skill_test.py
+++ tests/time_skill_test.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import locale
 import sys
 import unittest
 
@@ -10,6 +12,13 @@ from src.time_skill import Time_Skill
 class Time_Skill_Test(unittest.TestCase):
     def setUp(self) -> None:
         self.ts = Time_Skill()
+
+    def test_if_locale_was_set_to_default_in_init(self):
+        # 0 index provide country code
+        self.assertIn(locale.getdefaultlocale()[0], self.ts._locale)
+
+    def test_if_datetime_was_called_in_init(self):
+        self.assertEqual(self.ts._today.date(), datetime.today().date())
 
     def test_get_date_is_str_type(self):
         self.assertTrue(type(self.ts.get_date()) is str)
