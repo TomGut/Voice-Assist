0f6560c9b740ba816e57b14e07dc4bbafd99360e
tom <t-gutowski@o2.pl>
2022-03-15 18:23:54 +0100

    test(weather_skill): added setup

--- tests/weather_skill_test.py
+++ tests/weather_skill_test.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+from unittest.mock import Mock
 
 sys.path.append("..")
 
@@ -7,13 +8,14 @@ from src.weather_skill import Weather_Skill
 
 
 class Weather_Skill_Test(unittest.TestCase):
-    def test_city_name_gdansk_if_no_city_in_init(self):
+    def setUp(self) -> None:
         self.ws = Weather_Skill()
+
+    def test_city_name_gdansk_if_no_city_in_init(self):
         self.assertEqual(self.ws.city, "Gdańsk")
 
-    def test_city_namr_equal_to_provided(self):
-        self.ws = Weather_Skill(city="Warszawa")
-        self.assertEqual(self.ws.city, "Warszawa")
+    def test_if_temp_return_string(self):
+        self.assertIsInstance(self.ws.temp(), str)
 
 
 if __name__ == "__main__":
