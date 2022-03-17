fd8059805576701cbd99bb061f25830d0092b59e
tom <t-gutowski@o2.pl>
2022-03-17 22:48:52 +0100

    test(test_nltk_tools): add basic tests

--- tests/test_nltk_tools.py
+++ tests/test_nltk_tools.py
@@ -11,6 +11,15 @@ class Nltk_Tools_Test(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.nl_tools = Nltk_Tools()
         cls.sentence = "test sentence"
+        cls.bag = cls.nl_tools.bag_of_words(
+            cls.nl_tools.tokenize(cls.sentence), ["test", "test", "tes"]
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.nl_tools = None
+        cls.sentence = None
+        cls.bag = None
 
     def test_tokenize_words_separation_from_sentence(self):
         tokenized_sentence = self.nl_tools.tokenize(self.sentence)
@@ -21,6 +30,15 @@ class Nltk_Tools_Test(unittest.TestCase):
         tokenized_sentence = self.nl_tools.tokenize(self.sentence)
         self.assertIsInstance(tokenized_sentence[0], str)
         self.assertIsInstance(tokenized_sentence, list)
+
+    def test_bag_of_words_return_list_of_floats(self):
+        self.assertTrue(type(self.bag), list)
+        self.assertIsInstance(self.bag[0], float)
+
+    def test_bag_of_words_if_word_found_in_stemmed_sentence(self):
+        self.assertTrue(self.bag[0] == 1.0)
+        self.assertTrue(self.bag[1] == 1.0)
+        self.assertTrue(self.bag[2] == 0)
 
 
 if __name__ == "__main__":
