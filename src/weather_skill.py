9cd8d42f35c0a02578dec3e3d5f6d4302e803714
tom <t-gutowski@o2.pl>
2022-10-26 18:17:56 +0200

    fix(no api key): add methods return None if no API key provided. Apply pep8 class naming conventions.

--- src/weather_skill.py
+++ src/weather_skill.py
@@ -1,39 +1,43 @@
 import os
 from datetime import datetime
+from typing import Union
 
 from dotenv import load_dotenv
 from geopy import Nominatim
 from pyowm import OWM
 
 
-class Weather_Skill:
+class WeatherSkill:
     """
     Functionality to provide weather forecasting with usage of OpenWeather API.
     OpenWeather API key keept private - loaded from .env (see .env-example).
     """
 
     load_dotenv()
-    api_key = os.getenv("OPEN_WEATHER_APIKEY")
 
     def __init__(self, city=None) -> None:
-        self.ow = OWM(self.api_key)
-        self.mgr = self.ow.weather_manager()
-        locator = Nominatim(user_agent="bot")
+        self.api_key = os.getenv("OPEN_WEATHER_APIKEY")
+        if self.api_key != None:
+            self.ow = OWM(self.api_key)
+            self.mgr = self.ow.weather_manager()
+            locator = Nominatim(user_agent="bot")
 
-        # Possibilty to instantiate with None param.
-        if city == None:
-            self.city = "Gdańsk"
+            # Possibilty to instantiate with None param.
+            if city == None:
+                self.city = "Gdańsk"
+            else:
+                self.city = city
+
+            # Country needed for loc.
+            country = ", PL"
+            loc = locator.geocode(self.city + country)
+            self.lat = loc.latitude
+            self.lon = loc.longitude
+            self.fore = self.mgr.one_call(lat=self.lat, lon=self.lon)
         else:
-            self.city = city
+            return None
 
-        # Country needed for loc.
-        country = ", PL"
-        loc = locator.geocode(self.city + country)
-        self.lat = loc.latitude
-        self.lon = loc.longitude
-        self.fore = self.mgr.one_call(lat=self.lat, lon=self.lon)
-
-    def temp(self) -> str:
+    def temp(self) -> Union[str, None]:
         """
         Provides temp in celsius grade.
         forecats_daily[0] indicates present day.
@@ -41,14 +45,17 @@ class Weather_Skill:
         Returns:
             str: day midian temp.
         """
-        temp = (
-            self.fore.forecast_daily[0]
-            .temperature("celsius")
-            .get("day")
-        )
-        return str(temp)
+        if self.api_key != None:
+            temp = (
+                self.fore.forecast_daily[0]
+                .temperature("celsius")
+                .get("day")
+            )
+            return str(temp)
+        else:
+            return None
 
-    def sun_rise(self) -> str:
+    def sun_rise(self) -> Union[str, None]:
         """
         Provides sun rise hour.
         sun_rise - the [0] index in forecast_daily indicates
@@ -57,7 +64,10 @@ class Weather_Skill:
         Returns:
             str: sun rise hour.
         """
-        sun_rise = datetime.utcfromtimestamp(
-            self.fore.forecast_daily[0].sunrise_time()
-        ).strftime("%X")
-        return str(sun_rise)
+        if self.api_key != None:
+            sun_rise = datetime.utcfromtimestamp(
+                self.fore.forecast_daily[0].sunrise_time()
+            ).strftime("%X")
+            return str(sun_rise)
+        else:
+            return None
